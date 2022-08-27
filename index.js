0dc039b894bd22a644dd64065f75cc464dbc280a
thomasrcham <guseldorph@gmail.com>
2022-08-27 14:30:59 -0500

    commit tree f014aacf5569a9f70ccd052d2c0e062f7463ca56

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(tripFeet){
+  if (tripFeet <=400) {
+    return 'This one is on me!'
+  } else if (tripFeet >400 && tripFeet <=2000) {
+    return 'That will be twenty bucks.'
+  } else if (tripFeet >2000 && tripFeet <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+let answer = ""
+  
+function ternaryCheckCity(cityCheck){
+  answer = (cityCheck === 'NYC') ? `Ok, sounds good.` : "No go.";
+  return answer;
 }
+let response = ""
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 
+      'generous': response = "Thank you so much.";
+      return response;
+    case 
+      'not as generous': response = "Thank you.";
+      return response;
+    case
+      'thanks for everything': response = "Bye.";
+      return response;
+  }
 }
